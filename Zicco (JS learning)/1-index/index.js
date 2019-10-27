2a93619a8c40f805f21aa2c57f071aef56d966f8
rezatajari <reza.tajari70@gmail.com>
2019-10-27 17:02:52 +0330

    Index
    alert, button, get id

---
+++ Zicco (JS learning)/1-index/index.js
@@ -0,0 +1,5 @@
+alert("welcome reza to javascript man");
+document.write("salam pesarjan in this <br> message is hellow world");
+document.getElementById("Twoclickbyjscode").onclick=function(){
+    alert("show message with jscode click");
+}
