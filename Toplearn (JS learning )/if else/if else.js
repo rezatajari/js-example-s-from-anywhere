b839f62196ef9c59e732e04ffa7a78339196800c
rezatajari <reza.tajari70@gmail.com>
2019-10-31 06:22:12 +0330

    if else

---
+++ Toplearn (JS learning )/if else/if else.js
@@ -0,0 +1,13 @@
+var time = 10;
+if (time <= 10) {
+    alert("Godd Morning");
+}
+else if (time <= 18) {
+    alert("Good Day");
+}
+else if (time <= 20) {
+    alert("Good Evening");
+}
+else {
+    alert("Good Night");
+}
