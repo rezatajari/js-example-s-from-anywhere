f64548b1876b7147d12577cd4ab3ca13115a9124
rezatajari <reza.tajari70@gmail.com>
2019-10-31 21:49:36 +0330

    switch

---
+++ Toplearn (JS learning )/switch/switch.js
@@ -0,0 +1,22 @@
+switch (new Date().getDay()) {
+    case 0: {
+        alert("Sunday");
+        break;
+    }
+    case 1: {
+        alert("Monday");
+        break;
+    }
+    case 2: {
+        alert("Tuesday");
+        break;
+    }
+    case 3: {
+        alert("Wednesday");
+        break;
+    }
+    case 4: {
+        alert("Thursday");
+        break;
+    }
+}
