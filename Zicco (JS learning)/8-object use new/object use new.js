a009a410154476abd5d2403c1a7c2c1faa776798
rezatajari <reza.tajari70@gmail.com>
2019-11-04 06:34:09 +0330

    object use "new object()"

---
+++ Zicco (JS learning)/8-object use new/object use new.js
@@ -0,0 +1,9 @@
+var myphone = new Object();
+console.log(myphone);
+myphone.name = "samsung";
+myphone.family = "galexy";
+myphone.androidmoder = 8.4;
+myphone.brandnamewaranty = "avazhang";
+myphone.waranty = 5;
+console.log(myphone);
+var shiftobject = myphone;
