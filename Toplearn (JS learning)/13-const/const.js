172e0dec09cefbf2ced4575fc98e1c5eca0d28da
rezatajari <reza.tajari70@gmail.com>
2019-11-07 09:38:11 +0330

    const

---
+++ Toplearn (JS learning)/13-const/const.js
@@ -0,0 +1,12 @@
+const person = { name: "Iman", family: "Madaeny", age: 30 };
+console.log(person);
+person.number = "09121234567";
+console.log(person);
+
+{
+    const person = ["Iman", "Reza", "Ali", "Sara"];
+    console.log(person);
+    person.push("hossien");
+    console.log(person);
+
+}
