f1b48bc8e67bf611e482a1a1bcfa303d4339336d
rezatajari <reza.tajari70@gmail.com>
2019-12-24 08:50:00 +0330

    use events in ajax

---
+++ Toplearn (JS learning)/33-use events in ajax/use events in ajax.js
@@ -0,0 +1,18 @@
+var requst = new XMLHttpRequest();
+requst.open('GET', 'data.json');
+requst.onreadystatechange = function() {
+    if (requst.readyState === 4 && requst.status === 200) {
+        var items = JSON.parse(requst.responseText);
+
+        var output = "<ul>";
+
+        for (var key in items) {
+            console.log(items[key]);
+            output += "<li>" + items[key].name + " | " + items[key].bio + "</li>";
+        }
+
+        output += "</ul>";
+        document.getElementById('update').innerHTML = output;
+    }
+}
+requst.send();
