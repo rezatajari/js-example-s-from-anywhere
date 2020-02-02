c8503a4113644be4e6b75b04a0e477a20b928f57
rezatajari <reza.tajari70@gmail.com>
2020-02-02 11:58:48 +0330

    #counter prj\setinterval time and show messages

--- Roocket/2-counter/counter.js
+++ Roocket/2-counter/counter.js
@@ -3,6 +3,9 @@ let inputCounter = startBox.querySelector('#input-counter');
 let startCounter = startBox.querySelector('#start-counter');
 let errorElement = document.querySelector('#error-message');
 let timerCircle = document.querySelector('.c100');
+let timerNum = document.querySelector('.c100>span');
+let loadingMessage = document.querySelector('.message .loading');
+let successMessage = document.querySelector('.message .success');
 
 startCounter.addEventListener('click', function(e) {
     let secounds = parseInt(inputCounter.value)
@@ -16,4 +19,21 @@ startCounter.addEventListener('click', function(e) {
     errorElement.classList.remove('active');
     startBox.classList.remove('active')
     timerCircle.style.display = 'block';
+    timerNum.textContent = secounds;
+    loadingMessage.style.display = 'block';
+    successMessage.style.display = 'none';
+
+
+    let timerId = setInterval(() => {
+        if (secounds <= 1) {
+            clearInterval(timerId);
+            startBox.classList.add('active');
+            timerCircle.style.display = 'none';
+            loadingMessage.style.display = 'none';
+            successMessage.style.display = 'block';
+            inputCounter.value = '';
+        }
+        secounds -= 1;
+        timerNum.textContent = secounds;
+    }, 1000)
 })
