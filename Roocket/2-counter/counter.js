5576bd4bf308ac12966ba7f3f8b43c717a44c89e
rezatajari <reza.tajari70@gmail.com>
2020-02-06 11:58:15 +0330

    #counter prj\refactoring code\final prj

--- Roocket/2-counter/counter.js
+++ Roocket/2-counter/counter.js
@@ -1,49 +1,82 @@
 let startBox = document.querySelector('.start-box');
 let inputCounter = startBox.querySelector('#input-counter');
 let startCounter = startBox.querySelector('#start-counter');
-let errorElement = document.querySelector('#error-message');
 let timerCircle = document.querySelector('.c100');
 let timerNum = document.querySelector('.c100>span');
-let loadingMessage = document.querySelector('.message .loading');
-let successMessage = document.querySelector('.message .success');
+let lastPercent = 'p100';
+let originalSecounds, secounds, timerId;
 
 startCounter.addEventListener('click', function(e) {
-    let secounds = parseInt(inputCounter.value)
+    secounds = parseInt(inputCounter.value)
 
-    if (isNaN(secounds)) {
-        errorElement.textContent = "زمان را به درستی وارد کنید";
-        errorElement.classList.add('active');
+    if (isNaN(secounds)) return toggleErrorMessage({ show: true, message: 'زمان را به درستی وارد کنید' })
+
+    toggleErrorMessage({ show: false });
+    toggleStartBox({ show: false });
+    toggleLoadingMessage({ show: true });
+    togleTimer({ show: true, secounds });
+
+
+    originalSecounds = secounds;
+    timerCircle.classList.add(lastPercent);
+    timerId = setInterval(startTimer, 1000)
+})
+
+let startTimer = () => {
+    if (lastPercent) timerCircle.classList.remove(lastPercent);
+
+    if (secounds <= 0) {
+        clearInterval(timerId);
+        toggleStartBox({ show: true });
+        togleTimer({ show: false });
+        toggleLoadingMessage({ show: false })
         return;
     }
 
-    errorElement.classList.remove('active');
-    startBox.classList.remove('active')
-    timerCircle.style.display = 'block';
+
+    secounds -= 1;
     timerNum.textContent = secounds;
-    loadingMessage.style.display = 'block';
-    successMessage.style.display = 'none';
+    let percent = lastPercent = `p${Math.abs(Math.floor((((originalSecounds - secounds) / originalSecounds) * 100) - 100))}`
+    timerCircle.classList.add(percent);
 
-    let originalSecounds = secounds;
-    let lastPercent = 'p100';
-    timerCircle.classList.add(lastPercent);
-    let timerId = setInterval(() => {
-        if (lastPercent) timerCircle.classList.remove(lastPercent);
+}
 
-        if (secounds <= 1) {
-            clearInterval(timerId);
-            startBox.classList.add('active');
-            timerCircle.style.display = 'none';
-            loadingMessage.style.display = 'none';
-            successMessage.style.display = 'block';
-            inputCounter.value = '';
-            return;
-        }
+let toggleErrorMessage = ({ show, message }) => {
+    let errorElement = document.querySelector('#error-message');
+    if (show) {
+        errorElement.textContent = message;
+        errorElement.classList.add('active');
+    } else {
+        errorElement.classList.remove('active');
+    }
+}
 
+let toggleStartBox = ({ show }) => {
+    if (show) {
+        startBox.classList.add('active')
+        inputCounter.value = '';
+    } else {
+        startBox.classList.remove('active')
+    }
+}
 
-        secounds -= 1;
-        let percent = Math.abs(Math.floor((((originalSecounds - secounds) / originalSecounds) * 100) - 100));
-        lastPercent = `p${percent}`;
-        timerCircle.classList.add(`p${percent}`);
+let togleTimer = ({ show, secounds }) => {
+    if (show) {
+        timerCircle.style.display = 'block';
         timerNum.textContent = secounds;
-    }, 1000)
-})
+    } else {
+        timerCircle.style.display = 'none';
+    }
+}
+
+let toggleLoadingMessage = ({ show }) => {
+    let loadingMessage = document.querySelector('.message .loading');
+    let successMessage = document.querySelector('.message .success');
+    if (show) {
+        loadingMessage.style.display = 'block';
+        successMessage.style.display = 'none';
+    } else {
+        loadingMessage.style.display = 'none';
+        successMessage.style.display = 'block';
+    }
+}
