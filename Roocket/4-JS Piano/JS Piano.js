9d1b41172a5a40d0845c843e654f3685ac859a9d
rezatajari <reza.tajari70@gmail.com>
2020-02-14 11:58:00 +0330

    #JS Piano prj\add audio files & final prj

--- Roocket/4-JS Piano/JS Piano.js
+++ Roocket/4-JS Piano/JS Piano.js
@@ -1,8 +1,10 @@
 const note = document.querySelector('.nowplaying');
 const keys = document.querySelectorAll('.key');
+const hints = document.querySelectorAll('.hints');
 
 window.addEventListener('keydown', function(e) {
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
 
     if (!key) return;
 
@@ -10,11 +12,16 @@ window.addEventListener('keydown', function(e) {
 
     key.classList.add('playing');
     note.innerHTML = keyNote;
-
-    function removeTransition() {
-        key.classList.remove('playing')
-    }
-
-    keys.forEach(key => key.addEventListener('transitionend', removeTransition));
-
+    audio.currentTime = 0;
+    audio.play()
 });
+
+function removeTransition() {
+    this.classList.remove('playing')
+}
+
+hints.forEach(function(elm, index) {
+    elm.style = `transition-delay:${index*50}ms`
+});
+
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
