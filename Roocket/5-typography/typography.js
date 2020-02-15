54b1ddfaebae30ec2250005c0748fe2dfea753d4
rezatajari <reza.tajari70@gmail.com>
2020-02-15 15:26:04 +0330

    #typography prj\add textShodow & final prj

--- Roocket/5-typography/typography.js
+++ Roocket/5-typography/typography.js
@@ -8,6 +8,14 @@ hero.addEventListener('mousemove', function(event) {
 
     x += event.target.offsetLeft;
     y += event.target.offsetTop;
-    console.log(width, height);
 
+    const xWalk = Math.round((x / width * walk) - (walk / 2));
+    const yWalk = Math.round((y / height * walk) - (walk / 2));
+
+    text.style.textShadow = `
+        ${xWalk}px ${yWalk}px 0 rgba(238,82,83,.7),
+        ${xWalk*-1}px ${yWalk}px 0 rgba(52,31,151,.7),
+        ${yWalk}px ${xWalk*-1}px 0 rgba(243,104,224,.7),
+        ${yWalk*-1}px ${xWalk}px 0 rgba(254,202,87,.7)
+    `
 })
