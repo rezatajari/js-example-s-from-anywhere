054101b0ff42d3d05a0aba9e02758699b13f944a
rezatajari <reza.tajari70@gmail.com>
2020-02-18 12:52:36 +0330

    #drag and drop prj\create draggable class

---
+++ Roocket/6-drag and drop/draggable.js
@@ -0,0 +1,18 @@
+class draggable {
+    constructor(options) {
+        this.setupList(options);
+    }
+
+
+    setupList(options) {
+        let { list, el: element, template } = options;
+
+        if (!element) throw Error('the list is not exist');
+        if (!list) throw Error('the data is not exist');
+        if (!Array.isArray(list)) throw Error('the list is not an array, please insert an array');
+        if (!template) throw Error('please add a Template function');
+        if (typeof template !== "function") throw Error('please add a function as template');
+
+        list.forEach(item => element.innerHTML += template(item))
+    }
+}
