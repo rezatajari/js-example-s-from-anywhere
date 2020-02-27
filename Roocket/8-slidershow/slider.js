0edf1cb523f6382f6b58f46cdc80dfbcb130f36c
rezatajari <reza.tajari70@gmail.com>
2020-02-27 20:15:32 +0330

    #slidershow prj/add createNextAndPrevBtns & add createDots & add showSlides(number)

--- Roocket/8-slidershow/slider.js
+++ Roocket/8-slidershow/slider.js
@@ -19,4 +19,39 @@ class slider {
 
         this.sliders = [...sliderElement.children].filter(elm => elm.classList.contains(slideClass))
     }
+
+    createNextAndPrevBtns() {
+        let { el: sliderElement } = this.option;
+
+        sliderElement.insertAdjacentHTML('beforeend', `
+            <a class="next">&#10095;</a>
+            <a class="prev">&#10094;</a>
+        `);
+
+        sliderElement.querySelector('.next').addEventListener('click', () => this.incrementSlide());
+        sliderElement.querySelector('.prev').addEventListener('click', () => this.decrementSlide());
+    }
+
+    incrementSlide = () => this.showSlides(this.slideIndex += 1);
+    decrementSlide = () => this.showSlides(this.slideIndex - +1);
+
+    createDots() {
+        let { el: sliderElement } = this.option;
+
+        let dotElements = [...this.sliders].map((slider, index) => `<span class="dot" data-slide="${index+1}"></span>`);
+
+        let dots = document.createElement('div');
+        dots.classList.add('dots');
+        dots.innerHTML = `${dotElements.join('')}`;
+
+        sliderElement.after(dots);
+
+        this.dots = dots.querySelectorAll('.dot');
+        this.dots.forEach(dot => dot.addEventListener('click', e => console.log(e)))
+    }
+
+    showSlides(number) {
+        if (number > this.sliders.length) this.slideIndex = 1;
+        if (number < 1) this.slideIndex = this.sliders.length;
+    }
 }
